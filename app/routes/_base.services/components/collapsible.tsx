10bf9a241cc4ee37c85b84f15984366ad324629f
solitro <solitro.justin@gmail.com>
2024-02-28 10:33:28 -0500

    gradient on collapsibles

--- app/routes/_base.services/components/collapsible.tsx
+++ app/routes/_base.services/components/collapsible.tsx
@@ -14,6 +14,7 @@ export const Collapsible = forwardRef<HTMLDivElement, CollapsibleProps>(
 				ref={ref}
 				className='collapse collapse-plus mb-8 bg-secondary p-6 rounded-xl shadow-lg text-secondary-content'
 			>
+				<div className='absolute z-0 w-full h-full opacity-20 top-0 left-0 bg-gradient-to-tr from-transparent from-25% to-white' />
 				<input type='checkbox' defaultChecked={!collapsed} />
 				<div className='collapse-title'>
 					<h2 className='sm:text-3xl text-2xl font-semibold mb-4'>{title}</h2>
